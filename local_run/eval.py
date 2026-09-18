9e3f0c09c41e18f2faa3e3844f0c7cdd360d0722
Xiaosu Hu <xiaosuhu@MacBookAir.lan>
2026-09-18 11:54:11 -0400

    update: folder&file names

--- local_run/eval.py
+++ local_run/eval.py
@@ -5,10 +5,10 @@ import torch
 from torch.utils.data import DataLoader
 import yaml
 
-from project_local_run.src.data import EEGDataset
-from project_local_run.src.models.classifier import build_model
-from project_local_run.src.losses import build_loss
-from project_local_run.src.evaluation import validate
+from local_run.src.data import EEGDataset
+from local_run.src.models.classifier import build_model
+from local_run.src.losses import build_loss
+from local_run.src.evaluation import validate
 
 def load_cfg(path: str):
     with open(path, "r") as f:
